47f4a3662c255857f894f930caca4718c313e2b5
Никита <92018890+NikitaGaletaAVT042@users.noreply.github.com>
2024-09-08 18:44:50 +0300

    Add files via upload

---
+++ NichailDjangodb/projectdb/firstbdproject/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here
+class Book(models.Model):
+    book_ganer =models.CharField(max_length=100)
+    book_title=models.CharField(max_length=100)
+    book_page_count=models.IntegerField()
+    # def __str__(self):
+    #     return f'{self.book_ganer}-{self.book_title}'
